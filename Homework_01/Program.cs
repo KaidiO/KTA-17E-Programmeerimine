b9bd843c48ac7416f0b7a5b38ae8497d2a096223
KaidiO <32923395+KaidiO@users.noreply.github.com>
2017-11-01 14:13:05 +0200

    Add files via upload

---
+++ Homework_01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Homework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pangakontole sisselogimine
+
+            Console.WriteLine("Tere tulemast: ");
+            Console.WriteLine();
+
+            int pin = 0000;                                  
+            bool correct = false;
+            
+            while (!correct)
+            {
+                Console.WriteLine("Palun sisesta Pin kood: ");
+                string pin1 = Console.ReadLine();
+                int number = int.Parse(pin1);
+                
+                Console.WriteLine();
+
+                if (pin != number)
+                {
+                    Console.WriteLine("Sinu sisestatud PIN kood on vale. Proovi uuesti! ");
+                    Console.WriteLine();
+                }
+            
+                else
+                {
+                    Console.WriteLine("Tere tulemast oma pangakontole! ");
+                    Console.WriteLine();
+                    break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
