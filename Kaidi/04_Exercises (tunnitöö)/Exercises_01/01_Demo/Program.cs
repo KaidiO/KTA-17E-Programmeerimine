32067b1563a3759b13f1675df24f772c885f56ad
KaidiO <32923395+KaidiO@users.noreply.github.com>
2017-11-17 13:53:51 +0200

    Create something new

---
+++ Kaidi/04_Exercises (tunnitöö)/Exercises_01/01_Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta siia midagi, mis on sul mõttes hetkel!");
+            Console.ReadLine();
+
+            Console.WriteLine("Eks ole hea ennast väljendada :)");
+
+            Console.ReadKey();
+
+        }
+    }
+}
