1dc77b80d33d8cf228811a685070ee5c4be2da01
KaidiO <32923395+KaidiO@users.noreply.github.com>
2018-01-15 11:42:30 +0200

    Add files via upload

---
+++ Kaidi/Tunnikontrolli parandus/Valikud/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valikud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta siia nii palju kohanimesid kui tead! Vajuta X, kui oled lõpetanud");
+            List<string> names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Sisesta kohanimi: ");
+                string input = Console.ReadLine();
+                if (input == "x")
+                {
+                    break;
+                }
+
+                names.Add(input);
+
+                               
+            }
+            Console.WriteLine(String.Join(", ", names));
+
+            Random rnd = new Random();
+            int n = rnd.Next(0,names.Count());
+
+            
+            Console.WriteLine($"Tänaseks kohanimeks osutus:{names[n]} ");
+                       
+
+            Console.ReadLine();
+}   }   }
+
+
+
