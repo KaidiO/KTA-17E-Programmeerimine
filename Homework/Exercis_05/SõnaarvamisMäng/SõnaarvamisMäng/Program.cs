13ede5c0bdec0daa6625e5bbc6e7402f33d5490a
KaidiO <32923395+KaidiO@users.noreply.github.com>
2018-01-15 16:01:43 +0200

    Add files via upload

---
+++ Homework/Exercis_05/SõnaarvamisMäng/SõnaarvamisMäng/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SõnaarvamisMäng
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Arvuti valib suvalise sõna. Proovi see ära arvata.");
+
+            List<string> sõnad = new List<string>();
+
+            sõnad.Add("kaksik");
+            sõnad.Add("moos");
+            sõnad.Add("pulgakomm");
+            sõnad.Add("raamat");
+            sõnad.Add("arvuti");
+            sõnad.Add("tammepuu");
+            sõnad.Add("maja");
+            sõnad.Add("jalanõu");
+            sõnad.Add("kampsun");
+            sõnad.Add("aken");
+
+            Random rnd = new Random();                       
+            int n = rnd.Next(0, sõnad.Count);
+
+            
+            
+
+            string valitudSõna = PeidetudTähtedeArv(sõnad[n], '_');
+            Console.WriteLine();
+
+
+
+            Console.WriteLine($"Sõnaks osutus:{sõnad[n]} ");
+
+
+
+            Console.ReadLine();
+        }
+
+       
+
+        static string PeidetudTähtedeArv(string word, char mask)
+        {
+            char[] hidden = word.ToCharArray();
+            for (int i = 0; i < word.Length; i++)
+            {
+                hidden[i] = mask;
+            }
+            return new string(hidden);
+        }
+    }
+}
